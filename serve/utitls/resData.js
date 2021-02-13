0a320b4e45a22af0736934a7e6c6870118679207
admin <1570769179@qq.com>
2021-02-14 06:40:32 +0800

    工具·类

---
+++ serve/utitls/resData.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2021/2/9.
+ * 接口返回数据格式
+ */
+function resData(option){
+    let data = {
+        code:200,
+        data:{},
+        describe:"操作成功",
+        ...option
+    }
+    return data
+}
+module.exports = resData
